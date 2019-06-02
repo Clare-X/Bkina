60482fc1957ff99ef547039a79460cf3c3e97832
Yunqing Li <1814620279@qq.com>
2019-06-02 14:51:19 +0800

    Update UserMananger.hpp

--- UserMananger.hpp
+++ UserMananger.hpp
@@ -11,7 +11,30 @@ class UserManager
 private:
 	FileManager<USER,10240> File;
 	const int InitId=2019;
+	const int cLimit=1024;
+	int ModLimit=1024;
 	USER* x;bool AMode=false;
+    void ModeOn()
+    {
+        x=new USER[Siz];
+        if (Siz>ModLimit) ModLimit=Siz+13;
+        File.AllRead(x,1,Siz);
+        AMode=true;
+    }
+    void ModeOff()
+    {
+        if (AMode)
+        {
+            File.AlWrite(x,1,Siz);
+            delete[] x;
+            AMode=false;
+        }
+    }
+    void DoubleSpace()
+    {
+        ModLimit*=2;
+        x=(USER*)(realloc(x,ModLimit*sizeof(USER)));
+    }
 public:
 	size_t Siz;
 	UserManager()
@@ -21,32 +44,21 @@ public:
 	    x=nullptr;
 	}
 	~UserManager(){ModeOff();}
-	void ModeOn()
-    {
-        x=new USER[Siz];
-        File.AllRead(x,1,Siz);
-        AMode=true;
-    }
-    void ModeOff()
-    {
-	    if (AMode)
-        {
-	        File.AlWrite(x,1,Siz);
-	        delete[] x;
-	        AMode=false;
-        }
-    }
 	size_t Register(USER &User)
 	{
 		User.Priv=1+(Siz==0);
-		if (AMode) x[Siz]=User;
+		if (AMode)
+        {
+		    x[Siz]=User;
+		    if (Siz>ModLimit-10) DoubleSpace();
+        }
 		else File.Push_Back(&User,1);
 		++Siz;
 		return Siz+InitId-1;
 	}
 	int Login(const USER &User,size_t Id)
 	{
-        if (Siz>10240&&!AMode) ModeOn();
+        if (Siz>cLimit&&!AMode) ModeOn();
 		if (Id<=(InitId-1)||Id>(InitId-1)+Siz) return 0;
 		if (AMode)return x[Id-InitId].Passwd==User.Passwd;
 		USER X;
@@ -55,20 +67,21 @@ public:
 	}
 	int Query(USER &User,size_t Id)
 	{
-        if (Siz>10240&&!AMode) ModeOn();
+        if (Siz>cLimit&&!AMode) ModeOn();
 		if (Id<=(InitId-1)||Id>(InitId-1)+Siz) return 0;
 		if (AMode) User=x[Id-InitId];
-		File.Read(&User,Id-(InitId-1));
+		else File.Read(&User,Id-(InitId-1));
 		return 1;
 	}
 	int Modify_Profile(USER &User,size_t Id)
 	{
-        if (Siz>10240&&!AMode) ModeOn();
+        if (Siz>cLimit&&!AMode) ModeOn();
 		if (Id<=(InitId-1)||Id>(InitId-1)+Siz) return 0;
 		if (AMode)
         {
 		    User.Priv=x[Id-InitId].Priv;
 		    x[Id-InitId]=User;
+		    return 1;
         }
 		USER X;
 		File.Read(&X,Id-(InitId-1));
@@ -78,7 +91,7 @@ public:
 	}
 	int Modify_Privilege(size_t Id1,size_t Id2,int Priv)
 	{
-        if (Siz>10240&&!AMode) ModeOn();
+        if (Siz>cLimit&&!AMode) ModeOn();
 		if (Id1<=(InitId-1)||Id1>(InitId-1)+Siz) return 0;
 		if (Id2<=(InitId-1)||Id2>(InitId-1)+Siz) return 0;
 		if (AMode)
