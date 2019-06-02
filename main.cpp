3c7e97cf7a4de66b19cc0608de05043e6f9c8306
Yunqing Li <1814620279@qq.com>
2019-06-02 22:25:47 +0800

    Close

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     bool Exit=true;
     UserManager MainUser;
     TrainManager MainTrain;
-    std::cout<<std::fixed<<std::setprecision(2);
+    std::cout<<std::fixed<<std::setprecision(2);std::ios::sync_with_stdio(false);
 	//std::fstream In,Out;In.open("0.in",std::ios::in);Out.open("x.out",std::ios::out|std::ios::app);
     while (Exit)
     {
